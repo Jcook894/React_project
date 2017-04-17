1a162e01189dd9bd1654cc4b32cfd61ad6dd78f5
Jcook894 <jcook894@icloud.com>
2017-04-17 15:35:30 -0400

    Feat: added search bar class comp.

--- src/components/app.js
+++ src/components/app.js
@@ -3,15 +3,16 @@ import React, { Component } from 'react';
 
 const headerStyle = {
   background: "grey",
+  color: "white",
   textAlign: "center",
-  paddingTop: "10px"
+  padding: "10px"
 };
 
 export default class App extends Component {
 
   render() {
     return (
-      <div style={headerStyle}>Julians React Project</div>
+      <div style={headerStyle}> Bar App </div>
     );
   }
 }
