a7bade8493ea74231e5bdf750bc83c3db45c2010
Jcook894 <jcook894@icloud.com>
2017-04-20 23:50:35 -0400

    Feat: Added Search input to the search bar componenet.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,10 @@ import React, { Component } from 'react';
 class SearchBar extends Component{
   render(){
     return(
+      <div>
+        <input value="Hello"> </input>
         <button> Search for a Bar !</button>
+      </div>  
   );
  }
 }
